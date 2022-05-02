181363114a39bd2ecb3406061e77715162366505
Elliot Garcia <elliotgarciabrewer@gmail.com>
2022-05-02 12:05:16 +0200

    Correction BlankLabel

--- Atelier1/SB-part/SPWebAppStep1/src/main/java/com/sp/controller/PoneyDao.java
+++ Atelier1/SB-part/SPWebAppStep1/src/main/java/com/sp/controller/PoneyDao.java
@@ -12,7 +12,7 @@ import com.sp.model.Poney;
 public class PoneyDao {
 	private List<Poney> myPoneyList;
 	private Random randomGenerator;
-
+	
 	public PoneyDao() {
 		myPoneyList=new ArrayList<>();
 		randomGenerator = new Random();
@@ -20,13 +20,13 @@ public class PoneyDao {
 	}
 
 	private void createPoneyList() {
-  
+		
 		Poney p1=new Poney("John", "pink", "super pink", "http://ekladata.com/9-cPSlYvrenNHMVawFmf_gLx8Jw.gif");
 		Poney p2=new Poney("Roberto", "blue", "super lazy", "http://ekladata.com/JEVyY9DkwX4vVkakeBfikSyPROA.gif");
 		Poney p3=new Poney("Anna", "orange", "super music girl", "http://ekladata.com/fMJl--_v-3CmisaynTHju1DMeXE.gif");
 		Poney p4=new Poney("Angry Joe", "purple", "super angry power", "http://ekladata.com/AmbNNNvv-4YFEMZR8XD8e54WoHc.gif");
 		Poney p5=new Poney("Ursula", "green", "super cloning power", "http://ekladata.com/CXJhi2YLUbNz6__e0Ct6ZP-XOds.gif");
-
+		
 		myPoneyList.add(p1);
 		myPoneyList.add(p2);
 		myPoneyList.add(p3);
--- Atelier1/SB-part/SPWebAppStep1/src/main/java/com/sp/model/PoneyFormDTO.java
+++ Atelier1/SB-part/SPWebAppStep1/src/main/java/com/sp/model/PoneyFormDTO.java
@@ -1,30 +1,29 @@
 package com.sp.model;
 
-public class Poney  {
+public class PoneyFormDTO  {
 
 	private String color;
 	private String superPower;
 	private String name;
 	private String imgUrl;
 
-	public Poney() {
-		this.color = "";
-		this.superPower = "";
-		this.name = "";
-		this.imgUrl="";
-	}
-	public Poney(String name,String color,String superPower, String imgUrl) {
-		this.color = color;
-		this.superPower = superPower;
-		this.name = name;
-		this.imgUrl=imgUrl;
-	}
-	
 	public String getImgUrl() {
 		return imgUrl;
 	}
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
+	}
+	public PoneyFormDTO() {
+		this.color = "";
+		this.superPower = "";
+		this.name = "";
+		this.imgUrl="";
+	}
+	public PoneyFormDTO(String name,String color,String superPower, String imgUrl) {
+		this.color = color;
+		this.superPower = superPower;
+		this.name = name;
+		this.imgUrl=imgUrl;
 	}
 	public String getColor() {
 		return color;
