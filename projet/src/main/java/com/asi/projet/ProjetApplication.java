17319b604000aa71af831cf635973ccedc1f8c7a
Vick Foëx <vick.foex@cpe.fr>
2022-05-04 15:57:11 +0200

    add projet

---
+++ projet/src/main/java/com/asi/projet/ProjetApplication.java
@@ -0,0 +1,13 @@
+package com.asi.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetApplication.class, args);
+	}
+
+}
