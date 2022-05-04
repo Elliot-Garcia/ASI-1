b45ee7875ab4a300b7bef1cea14dd5d8d8c9e863
Vick Foëx <vick.foex@cpe.fr>
2022-05-04 16:29:32 +0200

    mise en place spring boot et maven+ gene proj

---
+++ projet/src/main/java/com/asi/projet/controller/RequestCrt.java
@@ -0,0 +1,19 @@
+package com.asi.projet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller 
+public class RequestCrt {
+
+	private static String messageLocal="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+
+	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("messageLocal", messageLocal);
+		return "index";
+	}
+
+}
---
+++ projet/src/main/java/com/asi/projet/controller/RequestLogin.java
@@ -0,0 +1,5 @@
+package com.asi.projet.controller;
+
+public class RequestLogin {
+
+}
