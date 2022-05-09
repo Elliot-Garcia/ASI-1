b2e76db51f7b8c65ff15926a61572da719be71db
Elliot Garcia <elliotgarciabrewer@gmail.com>
2022-05-09 09:20:35 +0200

    RestUserData

--- projet/src/main/java/com/asi/projet/userData/controller/RestUserData.java
+++ projet/src/main/java/com/asi/projet/userData/controller/RestUserData.java
@@ -1,5 +1,22 @@
 package com.asi.projet.userData.controller;
 
-public class RestUserData {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.asi.projet.authentification.controller.ServiceLogin;
+
+@RestController
+public class RestUserData {
+	
+	private final ServiceUserData sUserData;
+	
+	RestUserData(ServiceUserData serviceUserData) {
+		this.sUserData = serviceUserData;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/userdata")
+	public String getUserData() {
+		return sUserData.getUserData();
+	}
 }
