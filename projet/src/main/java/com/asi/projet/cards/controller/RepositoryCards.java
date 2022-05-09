ab7ece6ee51b2c4258be9162ff266443a384b2d4
Elliot Garcia <elliotgarciabrewer@gmail.com>
2022-05-09 12:07:32 +0200

    Def Cards Entity & repository for Templates

--- projet/src/main/java/com/asi/projet/cards/controller/RepositoryCards.java
+++ projet/src/main/java/com/asi/projet/cards/controller/RepositoryCards.java
@@ -1,10 +1,13 @@
 package com.asi.projet.cards.controller;
 
+import java.awt.List;
 
-public interface RepositoryCards extends CrudRepository<Card, Integer> {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import com.asi.projet.cards.model.Cards;
+
+public interface RepositoryCards extends CrudRepository<Cards, Integer> {
 
 	@Query("SELECT t.id_templates FROM templates t")
-	List<int> getTemplates();
-	
-	public List<int> getTemplates();
+	List getTemplates();
 }
--- projet/src/main/java/com/asi/projet/cards/controller/ServiceInitCards.java
+++ projet/src/main/java/com/asi/projet/cards/controller/ServiceInitCards.java
@@ -6,13 +6,11 @@ import org.springframework.stereotype.Service;
 public class ServiceInitCards {
 
 	private final RepositoryGetUser rGetUser;
-	private final RepositoryGetTemplates rGetTemplates;
-	private final RepositoryCreateCard rCreateCard;
+	private final RepositoryCards rCards;
 	
-	public ServiceInitCards(RepositoryCreateCard repositoryCreateCard, RepositoryGetTemplates repositoryGetTemplates, RepositoryGetUser repositoryGetUser) {
+	public ServiceInitCards(RepositoryCards repositoryCards, RepositoryGetUser repositoryGetUser) {
 		this.rGetUser = repositoryGetUser;
-		this.rGetTemplates = repositoryGetTemplates;
-		this.rCreateCard = repositoryCreateCard;
+		this.rCards = repositoryCards;
 	}
 	
 	/**
@@ -24,13 +22,13 @@ public class ServiceInitCards {
 		// Récupération de l'id du User
 	    int idUser = rGetUser.getUser['id'];
 	    // Récupération de la liste des ids des Templates des Cartes
-	    List<int> ListIdTemplates = rGetTemplates.getTemplates();
+	    List<int> ListIdTemplates = rCards.getTemplates();
 	    int nbTemplates = len(ListIdTemplates);
 	    
 	    for (i = 0; i < 5; i++) {
 	    	int rd = getRandomInt(nbTemplates);
 	    	int rdTemplateId = ListIdTemplates[rd];
-	    	rCreateCard.create(idUser, rdTemplateId);
+	    	rCards.createCard(idUser, rdTemplateId);
 	    }
 	}
 	
