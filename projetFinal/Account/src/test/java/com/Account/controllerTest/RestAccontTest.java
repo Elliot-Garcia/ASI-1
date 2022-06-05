3a468f0b8d0690b58c4ff6710032560834d819d1
Eliott-rjd <eliott.rajaud@gmail.com>
2022-06-05 13:40:55 +0200

    test

---
+++ projetFinal/Account/src/test/java/com/Account/controllerTest/RestAccontTest.java
@@ -0,0 +1,92 @@
+package com.Account.controllerTest;
+
+import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.Account.controller.RestAccount;
+import com.Account.controller.ServiceAccount;
+import com.Account.model.AccountBody;
+import com.AccountDTO.AccountDTO;
+import com.AccountDTO.AccountFormDTO;
+
+@RunWith(SpringRunner.class)	
+@WebMvcTest(value = RestAccount.class)
+@WithMockUser
+public class RestAccontTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private ServiceAccount studentService;
+
+	AccountBody user = new AccountBody("test", "UNIT", 5000);
+
+	String exampleAccountJson = "{\"id\":\"Spring\",\"login\":\"10Steps\",\"password\":\"psw\",\"balance\":\"1000\"}";
+
+	@Test
+	public void showUserInfo() throws Exception {
+
+		Mockito.when(
+				studentService.getUserInfo(Mockito.anyString())).thenReturn(user);
+
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+				"/account/1").accept(
+				MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+		System.out.println(result.getResponse());
+		String expected = "{id:Course1,login:Spring,password:10Steps,balance:1000}";
+
+		// {"id":"Course1","name":"Spring","description":"10 Steps, 25 Examples and 10K Students","steps":["Learn Maven","Import Project","First Example","Second Example"]}
+
+		JSONAssert.assertEquals(expected, result.getResponse()
+				.getContentAsString(), false);
+	}
+
+	
+	
+	@Test
+	public void addAccount() throws Exception {
+		AccountBody addUser = new AccountBody("unitAdd", "testadd",1000);
+
+		// studentService.addCourse to respond back with mockCourse
+		Mockito.when(
+				studentService.addAccount(Mockito.any(AccountFormDTO.class))).thenReturn(addUser);
+
+		// Send course as body to /students/Student1/courses
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.post("/account")
+				.accept(MediaType.APPLICATION_JSON).content(exampleAccountJson)
+				.contentType(MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+		MockHttpServletResponse response = result.getResponse();
+
+		assertEquals(HttpStatus.CREATED.value(), response.getStatus()	);
+
+		assertEquals("http://localhost/8081/account",
+				response.getHeader(HttpHeaders.LOCATION));
+
+	}
+	
+}
